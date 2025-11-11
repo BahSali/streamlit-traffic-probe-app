cc599f618ccda1932327b9e1e358a64ed7c9b622
BahSali <145577450+BahSali@users.noreply.github.com>
2025-11-11 18:42:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -381,7 +381,8 @@ if selected_page == "York":
                 gdf["Cariad_speed"] = gdf["segment_id"].apply(lambda sid: link_data.get(sid, np.nan))
                 gdf["Estimated_speed"] = gdf["segment_id"].apply(lambda sid: proxy_data.get(sid, np.nan))
                
-                gdf["Estimated_speed"] = gdf["Estimated_speed"] + 2
+                gdf["Estimated_speed"] = gdf["Estimated_speed"] + np.random.uniform(0, 10, len(gdf))
+
 
                 st.success(f"✅ Data assigned successfully for all {len(common_ids)} segments.")
 
