43ce43c915e101711fbe261554f1057197d61bcb
BahSali <145577450+BahSali@users.noreply.github.com>
2026-02-27 14:03:28 +0100

    Update brussels_controls.py

--- core/ui/brussels_controls.py
+++ core/ui/brussels_controls.py
@@ -58,34 +58,4 @@ def brussels_left_controls(
             key="bru_colorize_btn",
         )
 
-    return {"mode": mode, "filters": filters, "colorize_clicked": colorize}                    default=st.session_state.get("bru_seg_names", []),
-                    key="bru_seg_names",
-                )
-                filters["bus_ids"] = st.multiselect(
-                    "Bus ID(s)",
-                    options=bus_id_options,
-                    default=st.session_state.get("bru_bus_ids", []),
-                    key="bru_bus_ids",
-                )
-
-            elif mode == "Street":
-                filters["streets"] = st.multiselect(
-                    "Street(s)",
-                    options=street_options,
-                    default=st.session_state.get("bru_streets", []),
-                    key="bru_streets",
-                )
-
-            st.markdown("---")
-            col1, col2 = st.columns(2)
-            with col1:
-                submitted_apply = st.form_submit_button("Apply filters", use_container_width=True)
-            with col2:
-                submitted_colorize = st.form_submit_button("Colorize", use_container_width=True)
-
-    return {
-        "mode": mode,
-        "filters": filters,
-        "submitted_apply": submitted_apply,
-        "submitted_colorize": submitted_colorize,
-    }
+    return {"mode": mode, "filters": filters, "colorize_clicked": colorize}
